c36b7e790097e1b85d23e4962fcd57ce7bee898d
walexhenrique <walex999067@gmail.com>
2022-10-19 16:18:04 -0300

    Add register.html and login.html

--- list/settings.py
+++ list/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "list.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 
 
 # Default primary key field type
